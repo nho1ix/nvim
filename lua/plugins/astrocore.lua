25f598145eb596ed9076e9c957cb02d5b78ccac9
nho1ix <fromvictorvo@gmail.com>
2025-10-06 19:28:48 -0700

    initial commit: activate all files + fluoromachine

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -62,6 +62,12 @@ return {
         -- navigate buffer tabs
         ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+
+        -- for tmux
+        ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>" },
+        ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>" },
+        ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>" },
+        ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>" },
 
         -- mappings seen under group name "Buffer"
         ["<Leader>bd"] = {
